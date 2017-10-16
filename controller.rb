a61f7c90bf29d8ba0a5bebd7c0ae75e429b7f75d
Peter Crowther <petercrowther90@hotmail.com>
2017-10-16 21:19:24 +0100

    added text on rules to home erb file

--- controller.rb
+++ controller.rb
@@ -2,6 +2,10 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./models/game.rb')
 
+get '/' do
+  erb(:home)
+end
+
 get '/game/:hand1/:hand2' do
   game_play = Game.new(params[:hand1], params[:hand2])
   @game_play = game_play.game()
