7c3a039a834173a93b5f781867595d06e8801de8
Peter Crowther <petercrowther90@hotmail.com>
2017-10-16 20:52:59 +0100

    fixed code in game logic

--- models/game.rb
+++ models/game.rb
@@ -9,16 +9,17 @@ class Game
     case
     when @hand1 == "paper" && @hand2 == "scissors" || @hand1 == "scissors" &&  @hand2 == "paper"
       return "Scissors Wins!"
+    when @hand1 == "scissors" && @hand2 == "scissors" || @hand1 == "scissors" &&  @hand2 == "scissors"
+      return "Draw.."
     when @hand1 == "paper" && @hand2 == "rock" || @hand1 == "rock" &&  @hand2 == "paper"
       return "Paper Wins!"
     when @hand1 == "paper" && @hand2 == "paper" || @hand1 == "paper" &&  @hand2 == "paper"
       return "Draw.."
     when @hand1 == "rock" && @hand2 == "scissors" || @hand1 == "scissors" &&  @hand2 == "rock"
       return "Rock Wins!"
-    when @hand1 == "rock" && @hand2 == "paper" || @hand1 == "paper" &&  @hand2 == "rock"
-      return "Paper Wins!"
     when @hand1 == "rock" && @hand2 == "rock" || @hand1 == "rock" &&  @hand2 == "rock"
-      return "Rock Wins!"
+      return "Draw.."
+
     end
   end
 
