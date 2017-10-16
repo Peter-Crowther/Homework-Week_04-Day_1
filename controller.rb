ed1645a60675e59903c26f30795e9fff8094288e
Peter Crowther <petercrowther90@hotmail.com>
2017-10-16 21:02:07 +0100

    created layout file

--- controller.rb
+++ controller.rb
@@ -5,7 +5,7 @@ require_relative('./models/game.rb')
 get '/game/:hand1/:hand2' do
   game_play = Game.new(params[:hand1], params[:hand2])
   @game_play = game_play.game()
-  erb(:results)
+  erb(:result)
 end
 
 # get '/rock/paper' do
