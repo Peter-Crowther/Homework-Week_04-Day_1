a47f2d821997d597e4f991ae10c61d63f11127fc
Peter Crowther <petercrowther90@hotmail.com>
2017-10-16 20:35:04 +0100

    created game logic class

--- controller.rb
+++ controller.rb
@@ -1,41 +1,44 @@
 require('sinatra')
 require('sinatra/contrib/all')
+require_relative('./models/game.rb')
 
-
-
-
-get '/rock/paper' do
-  return "paper wins"
+get '/game/:hand1/:hand2' do
+  game_play = Game.new(params[:hand1], params[:hand2])
+  return game_play.game()
 end
 
-get '/rock/scissors' do
-  return "rock wins"
-end
-
-get '/rock/rock' do
-  return "it's a draw"
-end
-
-get '/scissors/paper' do
-  return "scissors win"
-end
-
-get '/scissors/rock' do
-  return "rock wins"
-end
-
-get '/scissors/scissors' do
-  return "its a draw"
-end
-
-get '/paper/scissors' do
-  return "scissors wins"
-end
-
-get '/paper/rock' do
-  return "paper wins"
-end
-
-get '/paper/paper' do
-  return "its a draw"
-end
+# get '/rock/paper' do
+#   return "paper wins"
+# end
+#
+# get '/rock/scissors' do
+#   return "rock wins"
+# end
+#
+# get '/rock/rock' do
+#   return "it's a draw"
+# end
+#
+# get '/scissors/paper' do
+#   return "scissors win"
+# end
+#
+# get '/scissors/rock' do
+#   return "rock wins"
+# end
+#
+# get '/scissors/scissors' do
+#   return "its a draw"
+# end
+#
+# get '/paper/scissors' do
+#   return "scissors wins"
+# end
+#
+# get '/paper/rock' do
+#   return "paper wins"
+# end
+#
+# get '/paper/paper' do
+#   return "its a draw"
+# end
--- models/game.rb
+++ models/game.rb
@@ -5,9 +5,21 @@ class Game
     @hand2 = hand2
   end
 
-  def
-
-
+  def game()
+    case
+    when @hand1 == "paper" && @hand2 == "scissors" || @hand1 == "scissors" &&  @hand2 == "paper"
+      return "Scissors Wins!"
+    when @hand1 == "paper" && @hand2 == "rock" || @hand1 == "rock" &&  @hand2 == "paper"
+      return "Paper Wins!"
+    when @hand1 == "paper" && @hand2 == "paper" || @hand1 == "paper" &&  @hand2 == "paper"
+      return "Draw.."
+    when @hand1 == "rock" && @hand2 == "scissors" || @hand1 == "scissors" &&  @hand2 == "rock"
+      return "Rock Wins!"
+    when @hand1 == "rock" && @hand2 == "paper" || @hand1 == "paper" &&  @hand2 == "rock"
+      return "Paper Wins!"
+    when @hand1 == "rock" && @hand2 == "rock" || @hand1 == "rock" &&  @hand2 == "rock"
+      return "Rock Wins!"
+    end
   end
 
 
